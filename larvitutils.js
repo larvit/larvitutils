4fd94262310b2ce5357c04854f11470023f84761
Lilleman <lilleman@larvit.se>
2015-01-23 17:02:57 +0100

    Added stuff

--- larvitutils.js
+++ larvitutils.js
@@ -16,3 +16,47 @@ exports.hrtimeToMs = function(prevTime, precision) {
 
 	return (diff[0] * 1000 + (diff[1] / 1000000)).toFixed(precision);
 };
+
+/**
+ * Get JSON data from an URL and format it for partial rendering in larvit framework
+ *
+ * @param str url - local to this server
+ * @param func callback(err, obj)
+ */
+exports.getPartialData = function(url, callback) {
+	var router  = require('larvitrouter')(),
+	    tmpReq  = {'url': url}, // Simulate a request object
+	    path    = require('path'),
+	    appPath = path.dirname(require.main.filename);
+
+	router.resolve(tmpReq, function(err) {
+		var controller;
+
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		if (tmpReq.controllerName === undefined) {
+			err = new Error('No controller name resolved for API partial data call: "' + url + '"');
+			callback(err);
+			return;
+		}
+
+		controller = require(path.join(appPath, '/controllers', url));
+
+		controller.run(tmpReq, {}, function(err, request, response, data) {
+			var returnData = {
+				'url': url,
+				'data': data
+			};
+
+			if (err) {
+				callback(err);
+				return;
+			}
+
+			callback(null, returnData);
+		});
+	});
+};
