263ea98d9379fd2a885382b023ceb64a6ea546cd
Filip Johansson <filip@zmartbag.com>
2019-09-06 17:05:51 +0200

    Added coverage and fixed failing tests.

--- test/setTimeout.ts
+++ test/setTimeout.ts
@@ -3,24 +3,23 @@ import { Utils } from '../src/index';
 
 const utils = new Utils();
 
-test('setTimeout() - Wait about 100ms', t => {
-	const start = new Date();
-	utils.setTimeout(100).then(() => {
-		// @ts-ignore: Do not know why this is making an error
-		const passedTime = new Date() - start;
-		t.equal((passedTime - 100) >= 0, true);
-		t.equal((passedTime - 110) < 0, true);
-		t.end();
-	});
+test('setTimeout() - Wait about 100ms', async t => {
+	const start = Date.now();
+	await utils.setTimeout(100);
+
+	const passedTime = Date.now() - start;
+	t.equal((passedTime - 100) >= 0, true);
+	t.equal((passedTime - 110) < 0, true);
+	t.end();
 });
 
-test('setTimeout() - Wait about 1ms', t => {
-	const start = new Date();
-	utils.setTimeout(1).then(() => {
-		// @ts-ignore: Do not know why this is making an error
-		const passedTime = new Date() - start;
-		t.equal((passedTime - 1) >= 0, true);
-		t.equal((passedTime - 10) < 0, true);
-		t.end();
-	});
+test('setTimeout() - Wait about 10ms', async t => {
+	const start = Date.now();
+	await utils.setTimeout(10);
+
+	const passedTime = Date.now() - start;
+
+	t.equal((passedTime - 1) >= 5, true);
+	t.equal((passedTime - 15) < 0, true);
+	t.end();
 });
